b6d7f7a554bfe4df18617173485c1418a99d87c0
Yehonal <yehonal.azeroth@gmail.com>
2017-06-19 17:46:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -66,16 +66,17 @@
     var realm = QueryString.realm || getCookie("realm");
 
     switch (realm) {
-        case "azerothshard":
-            document.cookie = "realm=azerothshard";
-            /* Edit with path of TC-JSON-API */
-            app.api = "http://azerothshard.org/modules/TC-JSON-API/public/index.php/";
-            break;
         case "newage":
             document.cookie = "realm=newage";
             /* Edit with path of TC-JSON-API */
-            app.api = "http://server.wownewage.com/TC-JSON-API/public/index.php/";
+            app.api = location.protocol+"//server.wownewage.com/TC-JSON-API/public/index.php/";
             break;
+        case "azerothshard":
+        default:
+            document.cookie = "realm=azerothshard";
+            /* Edit with path of TC-JSON-API */
+            app.api = location.protocol+"//azerothshard.org/modules/TC-JSON-API/public/index.php/";
+        break;
     }
 
 
